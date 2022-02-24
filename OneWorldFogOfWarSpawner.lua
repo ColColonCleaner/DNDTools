ca0c1f8558efe5c4b7cdd809afc4f454092e18f4
ColColonCleaner <danielgradinjan@gmail.com>
2022-02-23 20:01:07 -0600

    FOW Spawner 2.0
    #58

--- OneWorldFogOfWarSpawner.lua
+++ OneWorldFogOfWarSpawner.lua
@@ -17,14 +17,42 @@ end
 
 -- Handles clicks on the setup button
 function buttonClick_spawnFOW()
+    local bounds = getMapBounds(true)
+    bounds.y = 20
     spawnObject({
         type = "FogOfWar",
-        position = {0, 5.9, 0},
-        rotation = {0, 90, 0},
-        scale = {59, 10, 88.07},
-        sound = true,
-        callback_function = function(spawned_object)
-            log(spawned_object.getBounds())
-        end
+        position = {0, 10.85, 0},
+        rotation = {0, 0, 0},
+        scale = bounds,
+        sound = true
     });
+end
+
+function getOneWorldMap()
+    for _, obj in ipairs(getAllObjects()) do
+        if obj ~= self and obj ~= nil and obj.getName() == "_OW_vBase" then
+            return obj
+        end
+    end
+    return nil
+end
+
+function getMapBounds(debug)
+    local defaultBounds = {x = 88.07, y = 1, z = 52.02}
+    local oneWorldMap = getOneWorldMap()
+    if oneWorldMap ~= nil then
+        local oneWorldBounds = oneWorldMap.getBounds();
+        if oneWorldBounds.size.x > 10 then
+            print("Using OneWorld map bounds.")
+            return oneWorldBounds.size
+        end
+        if debug then
+            print("A OneWorld map is not deployed! Using default bounds.")
+        end
+        return defaultBounds
+    end
+    if debug then
+        print("OneWorld is not available! Using default bounds.")
+    end
+    return defaultBounds
 end
